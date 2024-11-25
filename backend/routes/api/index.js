b5c7040f027ace0a6360b6ecf6473cd15de9f212
fishstiicks <loketteshimmer@gmail.com>
2024-11-25 10:38:52 -0600

    Add error handlers

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -5,4 +5,10 @@ router.post('/test', function(req, res) {
     res.json({ requestBody: req.body });
   });
 
+  router.get('/test', function(req, res) {
+    res.cookie('XSRF-TOKEN', req.csrfToken());
+    res.send('Hello World!');
+  });
+
+
 module.exports = router;
